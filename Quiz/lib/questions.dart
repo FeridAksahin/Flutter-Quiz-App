71962719c14acd5080764424263686b7d59303a5
Ferid Akşahin <83434568+FeridAksahin@users.noreply.github.com>
2021-10-19 04:20:43 +0300

    Update questions.dart

--- Quiz/lib/questions.dart
+++ Quiz/lib/questions.dart
@@ -3,7 +3,7 @@ class Questions {
   String questionText;
   bool questionAnswer;
 
-  Questions({String q, bool a}) {// constructor
+  Questions({String q, bool a}) {
     questionText=q;
     questionAnswer=a;
   }
